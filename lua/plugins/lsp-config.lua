ca5fe68995aee4d5d98ac31d0920c30003f14971
frynnor <leolomtadze6002@gmail.com>
2024-10-06 05:46:51 +0400

    cmp update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,24 +3,28 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" }
+				ensure_installed = { "lua_ls" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', '<C-a>', vim.lsp.buf.code_action, {})
-		end
-	}
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<C-a>", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
