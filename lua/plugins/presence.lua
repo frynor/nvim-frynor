8435e4d169e4284044cef7d12fc42b40392deb2e
frynnor <leolomtadze6002@gmail.com>
2024-10-10 22:08:34 +0400

    Updated plugins

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,10 +1,23 @@
 return {
-	"andweeb/presence.nvim",
+	"IogaMaster/neocord",
+	event = "VeryLazy",
 	config = function()
-		require("presence").setup({
-			auto_update = true,
-			neovim_image_text = "Neovim",
-			main_image = "neovim",
+		require("neocord").setup({
+			logo = "auto",
+			logo_tooltip = nil,
+			main_image = "language",
+			log_level = nil,
+			show_time = true,
+			global_timer = false,
+
+			editing_text = "Editing %s",
+			file_explorer_text = "Browsing %s",
+			git_commit_text = "Committing changes",
+			plugin_manager_text = "Managing plugins",
+			reading_text = "Reading %s",
+			workspace_text = "Working on %s",
+			line_number_text = "Line %s out of %s",
+			terminal_text = "Using Terminal",
 		})
 	end
 }
